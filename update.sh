826fe546f7013d23a9ea737e9d24bc1ee18d4c8e
jerjjj <wuzhijun@jerjjj.cn>
2023-05-21 10:39:01 +0800

    Update:translate update.sh to chinese

--- update.sh
+++ update.sh
@@ -15,37 +15,37 @@ echo "                                               ";
 
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}[ERROR]\e[0m This script must be run as root."
+   echo -e "${RED}[ERROR]\e[0m 这个脚本必须以root运行"
    exit 1
 fi
 
 install_dir="/usr/share/hackingtool"
 # Change to the directory containing the install.sh script
-cd $install_dir || { echo -e "${RED}[ERROR]\e[0m Could not change to directory containing install.sh."; exit 1; }
+cd $install_dir || { echo -e "${RED}[ERROR]\e[0m 无法更改为包含install.sh的目录."; exit 1; }
 echo -e "${YELLOW}[*] Checking Internet Connection ..${NC}"
 echo "";
 if curl -s -m 10 https://www.google.com > /dev/null || curl -s -m 10 https://www.github.com > /dev/null; then
-    echo -e "${GREEN}[✔] Internet connection is OK [✔]${NC}"
+    echo -e "${GREEN}[✔] 网络连接正常 [✔]${NC}"
     echo ""
 else
-    echo -e "${RED}[✘] Please check your internet connection[✘]"
+    echo -e "${RED}[✘] 请检查你的网络连接是否通畅[✘]"
     echo ""
     exit 1
 fi
-echo -e "[*]Marking hackingtool directory as safe-directory"
+echo -e "[*]将hackingtool目录标记为安全目录"
 git config --global --add safe.directory $install_dir
 # Update the repository and the tool itself
-echo -e "${BLUE}[INFO]\e[0m Updating repository and tool..."
+echo -e "${BLUE}[INFO]\e[0m 升级工具中..."
 if ! sudo git pull; then
-    echo -e "${RED}[ERROR]\e[0m Failed to update repository or tool."
+    echo -e "${RED}[ERROR]\e[0m 升级工具失败."
     exit 1
 fi
 
 # Re-run the installation script
-echo -e "${GREEN}[INFO]\e[0m Running installation script..."
+echo -e "${GREEN}[INFO]\e[0m 运行安装脚本..."
 if ! sudo bash install.sh; then
-    echo -e "${RED}[ERROR]\e[0m Failed to run installation script."
+    echo -e "${RED}[ERROR]\e[0m 运行安装脚本失败."
     exit 1
 fi
 
-echo -e "${GREEN}[SUCCESS]\e[0m Tool updated successfully."
+echo -e "${GREEN}[SUCCESS]\e[0m 工具升级成功."
